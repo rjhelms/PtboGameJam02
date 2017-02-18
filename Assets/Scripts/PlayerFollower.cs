973b259554e76eda6d7485dc87fd8722b625aeff
Rob Hailman <robhailman@gmail.com>
2017-02-18 16:51:57 -0500

    Add camera following player, maybe

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour {
+
+	public Actor Player;
+	public float CameraZOffset = -10f;
+	public float LerpSpeed = 1f;
+
+	private Vector2 world_position;
+	// Use this for initialization
+	void Start () {
+		world_position = Player.WorldPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		world_position = Vector2.Lerp(
+						     world_position, 
+							 Player.WorldPosition, 
+							 Time.deltaTime * LerpSpeed);
+		Vector3 new_position = new Vector3(
+							       Mathf.RoundToInt(world_position.x),
+								   Mathf.RoundToInt(world_position.y),
+								   CameraZOffset);
+		transform.position = new_position;
+	}
+}
