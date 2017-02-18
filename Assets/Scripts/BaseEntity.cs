b9672334db9f1ceba2e8c5b9c7e3bdb77c1a1c52
Rob Hailman <robhailman@gmail.com>
2017-02-18 12:25:45 -0500

    Add player movement
    Or at least, an imitation thereof.

---
+++ Assets/Scripts/BaseEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEntity : MonoBehaviour {
+
+	private Vector2 worldPosition;
+	private Vector2 screenPosition;
+	public Vector2 WorldPosition
+	{
+		get { return worldPosition;}
+		set { worldPosition = value;}
+	}
+
+	// Use this for initialization
+	void Start () {
+		worldPosition = (Vector2)transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		screenPosition.x = Mathf.RoundToInt(worldPosition.x);
+		screenPosition.y = Mathf.RoundToInt(worldPosition.y);
+		transform.position = screenPosition;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public BaseEntity Player;
+	public float MoveSpeed = 120.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DoMovement();
+	}
+
+	void FixedUpdate () {
+	}
+
+	void DoMovement() {
+		float x_movement = Input.GetAxis("Horizontal") * MoveSpeed;
+		float y_movement = Input.GetAxis("Vertical") * MoveSpeed;
+		Vector2 movement = new Vector2(x_movement, y_movement)
+						   		* Time.deltaTime;
+		Player.WorldPosition += movement;
+	}
+}
