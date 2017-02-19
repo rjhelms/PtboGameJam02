efc35719563833068af96c5d71cd5f7c63b81d27
Rob Hailman <robhailman@gmail.com>
2017-02-18 19:38:53 -0500

    Fix StaticEntity performance
    StaticEntity was hosing framerate by calling empty Update method 10000
    times a frame. By not inheriting BaseEntity, we can just not have an
    Update method greatly improving framerate

--- Assets/Scripts/StaticEntity.cs
+++ Assets/Scripts/StaticEntity.cs
@@ -1,9 +1,13 @@
-public class StaticEntity : BaseEntity {
-    protected override void Start ()
+using UnityEngine;
+public class StaticEntity : MonoBehaviour {
+	[SerializeField]
+	private Vector2 screenPositionOffset;
+    
+    void Start ()
     {
-        base.Start();
-        base.Update();
+        Vector2 worldPosition = (Vector2)transform.position;
+		Vector2 screenPosition = new Vector2(Mathf.RoundToInt(worldPosition.x),
+                                             Mathf.RoundToInt(worldPosition.y));
+		transform.position = screenPosition + screenPositionOffset;
     }
-
-    protected override void Update () {}
 }
